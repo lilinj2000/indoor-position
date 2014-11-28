279b170048e4d8d52f90263d58c6addf45cef8a8
lilinj2000 <lilinj2000@163.com>
2014-11-28 02:50:59 +0000

    finish the data process

--- wifi/matlab/wifiDataProcess.m
+++ wifi/matlab/wifiDataProcess.m
@@ -1,19 +1,9 @@
-
-function result = wifiDataProcess (grid_data, ap_data)
-%WIFIDATAPROCESS process the wifi base data, evaluate the result
-% Function WIFIDATAPROCESS analysis the gridData and the apData,
-% this function will random positioning 100 times for each grid,
-% then output each grid hit probability and the max error distance,
-% the positioning info each time of each grid.
+% Script file: wifiDataProcess.m 
 %
-% Calling sequence:
-% res = wifiDataProcess(grid_data, ap_data)
+% Purpose:
+% Base on the wifi data, and simulator positionging,
+% output the positioning result
 %
-% Define variables:
-% grid_data -- each grid the base wifi info
-% ap data -- each grid the real collected wifi info
-% result -- each grid hit probability, max error distance, 
-%           each position
 %
 % Record of revisions:
 % Date Pragrammer Description of change
@@ -21,22 +11,51 @@ function result = wifiDataProcess (grid_data, ap_data)
 % 11/27/2014 linjiang li Original code
 %
 
+clear;
+close all;
+
+load apData;
+load gridData;
+
 % sort the grid data
-grid_data = sortrows(grid_data);
+grid_data = sortrows(gridFile);
 
-for ii=1:size(grid_data, 1)
-    base_sig = grid_data(ii, 3:5);
-    base_loc = grid_data(ii, 1:2);
-    
-    % group the ap data
-    [ap1, ap2, ap3] = groupData(base_loc, ap_data);
-    
-    
-    % random select ap data
-    rand_num = random('Normal',0, 1000, 100, 3);
-    
-end
-    
 result = [];
+all_location = [];
 
+for jj=1:10
+    for ii=1:size(grid_data, 1)
+        base_loc = grid_data(ii, 1:2);
+
+        % group the ap data
+        [ap1, ap2, ap3] = groupAPData(base_loc, apFile);
+
+        % do positioning
+        [distr, max_dist, location] = positioning (base_loc, grid_data, ap1, ap2, ap3);
+
+        result = [result; base_loc, distr, max_dist];
+
+        all_location = [all_location; repmat(base_loc, size(location, 1), 1), location]; 
+    end
 end
+    
+figure;
+cdfplot(result(:,3));
+title('hit probability distr');
+
+figure;
+cdfplot(result(:,4));
+title('max error distance distr');
+
+figure;
+hold on;
+base_loc = [2, 3];
+for ii=1:size(all_location, 1)
+    if base_loc==all_location(ii, 1:2)
+        x = [all_location(ii, 1); all_location(ii, 3)];
+        y = [all_location(ii, 2); all_location(ii, 4)];
+        plot(x, y);
+    end
+end
+grid on;
+
