8b1e94f152e1eff4a5063325c23b004b552a0f6c
lilinj2000 <lilinj2000@163.com>
2014-12-11 04:42:17 +0000

    update the log format

--- wifi/location/server.py
+++ wifi/location/server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import json
 import logging
 
@@ -8,19 +10,27 @@ import grid
 
 class Location:
     def GET(self):
+        logging.info("do GET!!!")
+
         return "Hello, world!"
 
     def POST(sef):
+        logging.info("do POST!!!")
+
         try:
             data = web.data()
+            logging.info('the data received is %s', data)
 
             request = json.loads(data)
 
             loc = grid_file.positioning(request)
 
-            response = "{x:" + str(loc[0]) + "," + "y:" + str(loc[1]) + "}"
+            response = {}
+            response["x"] = str(loc[0])
+            response["y"] = str(loc[1])
+            logging.info("the response is %s", repr(response))
 
-            return response
+            return repr(response)
 
             # request_json = json.dumps(request, sort_keys=True, indent=4)
 
@@ -28,6 +38,9 @@ class Location:
             return web.badrequest();
 
 if __name__=="__main__":
+    # logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
+
     urls = (
         '/location', 'Location'
     )
