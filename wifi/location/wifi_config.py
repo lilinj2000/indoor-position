0722228a22015b7e3db2624dec2b1a40709320ed
lilinj2000 <lilinj2000@163.com>
2015-02-15 03:16:30 +0000

    wifi config done

---
+++ wifi/location/wifi_config.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""configure the wifi info"""
+
+__author__ = "Linjiang Li (lilinj2000@gmail.com)"
+__version__ = "$Revision: 1.0 $"
+__date__ = "$Date: 2015/2/15 14:40:20 $"
+__copyright__ = "Copyright (c) 2001 Linjiang Li"
+__license__ = "Python"
+
+
+import json
+
+
+def config_wifi():
+    """generate the ap config info"""
+
+    mac_1 = {"Mac": "b4:b5:2f:4d:51:11", "Level": -62, "Probability_of_TV": 0.1, "Probability_of_RDS": 0.9, "Weight": 1}
+    mac_2 = {"Mac": "b4:b5:2f:4d:51:10", "Level": -61, "Probability_of_TV": 0.1, "Probability_of_RDS": 0.9, "Weight": 1}
+    mac_3 = {"Mac": "b4:b5:2f:4d:c1:91", "Level": -79, "Probability_of_TV": 0.2, "Probability_of_RDS": 0.9, "Weight": 1}
+    mac_4 = {"Mac": "b4:b5:2f:4d:c1:90", "Level": -79, "Probability_of_TV": 0.2, "Probability_of_RDS": 0.9, "Weight": 1}
+
+    wifi_all = dict((item["Mac"], item) for item in
+                    [mac_1, mac_2, mac_3, mac_4])
+
+    # print aps
+    # print json.dumps(aps, sort_keys=True, indent=4)
+    with open("wifi.cfg", "w") as wifi_file:
+        json.dump(wifi_all, wifi_file, sort_keys=True, indent=4)
+        wifi_file.close()
+
+if __name__ == "__main__":
+    config_wifi()
